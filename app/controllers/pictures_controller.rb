6ba54366c4a74d7508c533e542f2c89698ec5983
deniseb89 <denise_b89@hotmail.com>
2014-12-06 16:51:15 -0500

    populates views and controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,31 +1,44 @@
 class PicturesController < ApplicationController
 
   def index
-
+    @pictures = Picture.all
   end
 
   def new
-
+    @picture = Picture.new
   end
 
   def create
-
+    @picture = Picture.create(picture_params)
+    redirect_to picture_path(@picture)
   end
 
   def edit
-
+    @picture = Picture.find(params[:id])
   end
 
   def update
+    picture = Picture.find(params[:id])
+    picture.update(picture_params)
 
+    redirect_to picture_path(picture)
   end
 
   def show
-
+    @picture = Picture.find(params[:id])
   end
 
   def destroy
+    picture = Picture.find(params[:id])
+    picture.destroy
 
+    redirect_to picture_path
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image)
   end
 
 end
